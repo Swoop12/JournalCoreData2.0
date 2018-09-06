4b1ab38e4ddbe1cf7b1c45164053e1b63f3edcd4
Trevor Adcock <trevor.adcock@me.com>
2018-09-06 10:45:54 -0600

    Initial Commit

---
+++ JournalFetchedResultsController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalFetchedResultsController
+//
+//  Created by DevMountain on 9/6/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
